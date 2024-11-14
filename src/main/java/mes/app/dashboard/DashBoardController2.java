4c5651c6a905bfe2a07193cfa71cb845958a2057
LeeJaeWook <ljw960214@naver.com>
2024-11-14 13:29:15 +0900

    20241114_이재욱_dashboard작업

---
+++ src/main/java/mes/app/dashboard/DashBoardController2.java
@@ -0,0 +1,99 @@
+package mes.app.dashboard;
+
+import mes.app.dashboard.service.DashBoardService;
+import mes.app.dashboard.service.DashBoardService2;
+import mes.domain.entity.User;
+import mes.domain.entity.actasEntity.TB_DA006W_PK;
+import mes.domain.model.AjaxResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/dashboard2")
+public class DashBoardController2 {
+    @Autowired
+    private DashBoardService2 dashBoardService2;
+
+    // 작년 1월1일부터 작년오늘날자까지 상태별 건수
+    @GetMapping("/LastYearCnt")
+    private AjaxResult LastYearCnt() {
+
+        List<Map<String, Object>> items = this.dashBoardService2.LastYearCnt();
+
+        AjaxResult result = new AjaxResult();
+        result.data = items;
+
+        return result;
+    }
+    // 올해 1월 1일부터 오늘 날짜까지의 상태별 건수
+    @GetMapping("/ThisYearCnt")
+    private AjaxResult ThisYearCnt() {
+
+        List<Map<String, Object>> items = this.dashBoardService2.ThisYearCnt();
+
+        AjaxResult result = new AjaxResult();
+        result.data = items;
+
+        return result;
+    }
+
+    // 올해 1월 1일부터 오늘 날짜까지의 월별 건수
+    @GetMapping("/ThisYearCntOfMonth")
+    private AjaxResult ThisYearCntOfMonth() {
+
+        List<Map<String, Object>> items = this.dashBoardService2.ThisYearCntOfMonth();
+
+        AjaxResult result = new AjaxResult();
+        result.data = items;
+
+        return result;
+    }
+
+    // 작년 1월 1일부터 작년 오늘 날짜까지의 월별 건수
+    @GetMapping("/LastYearCntOfMonth")
+    private AjaxResult LastYearCntOfMonth(Authentication auth) {
+        User user = (User) auth.getPrincipal();
+        String username = user.getUsername();
+        Map<String, Object> userInfo = dashBoardService2.getUserInfo(username);
+        TB_DA006W_PK tbDa006WPk = new TB_DA006W_PK();
+        tbDa006WPk.setSpjangcd("ZZ");
+        tbDa006WPk.setCustcd((String) userInfo.get("custcd"));
+
+        List<Map<String, Object>> items = this.dashBoardService2.LastYearCntOfMonth();
+
+        AjaxResult result = new AjaxResult();
+        result.data = items;
+
+        return result;
+    }
+
+    // 올해 당월 1일 부터 오늘까지 일별 건수
+    @GetMapping("/ThisMonthCntOfDate")
+    private AjaxResult ThisMonthCntOfDate() {
+
+        List<Map<String, Object>> items = this.dashBoardService2.ThisMonthCntOfDate();
+
+        AjaxResult result = new AjaxResult();
+        result.data = items;
+
+        return result;
+    }
+
+    // 올해 전월 1일 부터 전월 오늘 날자까지 일별 건수
+    @GetMapping("/LastMonthCntOfDate")
+    private AjaxResult LastMonthCntOfDate() {
+
+        List<Map<String, Object>> items = this.dashBoardService2.LastMonthCntOfDate();
+
+        AjaxResult result = new AjaxResult();
+        result.data = items;
+
+        return result;
+    }
+}
---
+++ src/main/java/mes/app/dashboard/service/DashBoardService2.java
@@ -0,0 +1,211 @@
+package mes.app.dashboard.service;
+
+import mes.domain.services.SqlRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class DashBoardService2 {
+    @Autowired
+    SqlRunner sqlRunner;
+
+    // username으로 cltcd, cltnm, saupnum, custcd 가지고 오기
+    public Map<String, Object> getUserInfo(String username) {
+        MapSqlParameterSource dicParam = new MapSqlParameterSource();
+
+        String sql = """
+                select custcd,
+                        cltcd,
+                        cltnm
+                FROM TB_XCLIENT
+                WHERE saupnum = :username
+                """;
+        dicParam.addValue("username", username);
+        Map<String, Object> userInfo = this.sqlRunner.getRow(sql, dicParam);
+        return userInfo;
+    }
+
+    public List<Map<String, Object>> LastYearCnt() {
+
+        String sql = """
+                   DECLARE @Today DATE = CAST(GETDATE() AS DATE);
+                DECLARE @LastYearStart DATE = DATEADD(YEAR, -1, CAST(CONCAT(YEAR(@Today), '-01-01') AS DATE));
+                DECLARE @LastYearEnd DATE = @Today;
+                DECLARE @ThisYearStart DATE = CAST(CONCAT(YEAR(@Today), '-01-01') AS DATE);
+                DECLARE @ThisYearEnd DATE = @Today;
+                
+                -- 작년 1월 1일부터 작년 오늘 날짜까지의 행 개수
+                SELECT COUNT(*) AS LastYearCount
+                FROM ERP_SWSPANEL1.dbo.TB_DA006W
+                WHERE CAST(reqdate AS DATE) BETWEEN @LastYearStart AND @LastYearEnd;
+                
+                -- 올해 1월 1일부터 오늘 날짜까지의 행 개수
+                SELECT COUNT(*) AS ThisYearCount
+                FROM ERP_SWSPANEL1.dbo.TB_DA006W
+                WHERE CAST(reqdate AS DATE) BETWEEN @ThisYearStart AND @ThisYearEnd;
+					""";
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+
+    public List<Map<String, Object>> ThisYearCnt() {
+
+        String sql = """
+                   DECLARE @Today DATE = CAST(GETDATE() AS DATE);
+                DECLARE @LastYearStart DATE = DATEADD(YEAR, -1, CAST(CONCAT(YEAR(@Today), '-01-01') AS DATE));
+                DECLARE @LastYearEnd DATE = @Today;
+                DECLARE @ThisYearStart DATE = CAST(CONCAT(YEAR(@Today), '-01-01') AS DATE);
+                DECLARE @ThisYearEnd DATE = @Today;
+                
+                -- 작년 1월 1일부터 작년 오늘 날짜까지의 행 개수
+                SELECT COUNT(*) AS LastYearCount
+                FROM ERP_SWSPANEL1.dbo.TB_DA006W
+                WHERE CAST(reqdate AS DATE) BETWEEN @LastYearStart AND @LastYearEnd;
+                
+                -- 올해 1월 1일부터 오늘 날짜까지의 행 개수
+                SELECT COUNT(*) AS ThisYearCount
+                FROM ERP_SWSPANEL1.dbo.TB_DA006W
+                WHERE CAST(reqdate AS DATE) BETWEEN @ThisYearStart AND @ThisYearEnd;
+					""";
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+
+    public List<Map<String, Object>> ThisYearCntOfMonth() {
+
+        String sql = """
+                   DECLARE @Today DATE = CAST(GETDATE() AS DATE);
+                              DECLARE @LastYearStart DATE = DATEADD(YEAR, -1, CAST(CONCAT(YEAR(@Today), '-01-01') AS DATE));
+                              DECLARE @ThisYearStart DATE = CAST(CONCAT(YEAR(@Today), '-01-01') AS DATE);
+                
+                              -- 작년 월별 행 개수
+                              SELECT\s
+                                  MONTH(CAST(reqdate AS DATE)) AS Month,
+                                  COUNT(*) AS LastYearCount
+                              FROM\s
+                                  ERP_SWSPANEL1.dbo.TB_DA006W
+                              WHERE\s
+                                  CAST(reqdate AS DATE) BETWEEN @LastYearStart AND @Today
+                              GROUP BY\s
+                                  MONTH(CAST(reqdate AS DATE))
+                              ORDER BY\s
+                                  Month;
+                
+                              -- 올해 월별 행 개수
+                              SELECT\s
+                                  MONTH(CAST(reqdate AS DATE)) AS Month,
+                                  COUNT(*) AS ThisYearCount
+                              FROM\s
+                                  ERP_SWSPANEL1.dbo.TB_DA006W
+                              WHERE\s
+                                  CAST(reqdate AS DATE) BETWEEN @ThisYearStart AND @Today
+                              GROUP BY\s
+                                  MONTH(CAST(reqdate AS DATE))
+                              ORDER BY\s
+                                  Month;
+					""";
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+
+    public List<Map<String, Object>> LastYearCntOfMonth() {
+
+        String sql = """
+                   DECLARE @Today DATE = CAST(GETDATE() AS DATE);
+                              DECLARE @LastYearStart DATE = DATEADD(YEAR, -1, CAST(CONCAT(YEAR(@Today), '-01-01') AS DATE));
+                              DECLARE @ThisYearStart DATE = CAST(CONCAT(YEAR(@Today), '-01-01') AS DATE);
+                
+                              -- 작년 월별 행 개수
+                              SELECT\s
+                                  MONTH(CAST(reqdate AS DATE)) AS Month,
+                                  COUNT(*) AS LastYearCount
+                              FROM\s
+                                  ERP_SWSPANEL1.dbo.TB_DA006W
+                              WHERE\s
+                                  CAST(reqdate AS DATE) BETWEEN @LastYearStart AND @Today
+                              GROUP BY\s
+                                  MONTH(CAST(reqdate AS DATE))
+                              ORDER BY\s
+                                  Month;
+                
+                              -- 올해 월별 행 개수
+                              SELECT\s
+                                  MONTH(CAST(reqdate AS DATE)) AS Month,
+                                  COUNT(*) AS ThisYearCount
+                              FROM\s
+                                  ERP_SWSPANEL1.dbo.TB_DA006W
+                              WHERE\s
+                                  CAST(reqdate AS DATE) BETWEEN @ThisYearStart AND @Today
+                              GROUP BY\s
+                                  MONTH(CAST(reqdate AS DATE))
+                              ORDER BY\s
+                                  Month;
+					""";
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+
+    public List<Map<String, Object>> ThisMonthCntOfDate() {
+
+        String sql = """
+                   SELECT
+                       hd.reqdate,
+                       hd.ordflag,
+                       COUNT(*) AS ordflag_count
+                   FROM
+                       TB_DA006W hd
+                   WHERE
+                       hd.custcd = @custcd
+                       AND hd.spjangcd = @spjangcd
+                       AND hd.reqdate BETWEEN FORMAT(DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), 1), 'yyyyMMdd')
+                                           AND FORMAT(GETDATE(), 'yyyyMMdd')
+                   GROUP BY
+                       hd.reqdate,
+                       hd.ordflag
+                   ORDER BY
+                       hd.reqdate;
+					""";
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+
+    public List<Map<String, Object>> LastMonthCntOfDate() {
+
+        String sql = """
+            SELECT
+              hd.reqdate,
+              hd.ordflag,
+              COUNT(*) AS ordflag_count
+            FROM
+              TB_DA006W hd
+            WHERE
+              hd.custcd = :custcd
+              AND hd.spjangcd = :spjangcd
+              AND hd.reqdate BETWEEN FORMAT(DATEFROMPARTS(YEAR(DATEADD(MONTH, -1, GETDATE())), MONTH(DATEADD(MONTH, -1, GETDATE())), 1), 'yyyyMMdd')
+                                  AND FORMAT(EOMONTH(DATEADD(MONTH, -1, GETDATE())), 'yyyyMMdd')
+            GROUP BY
+              hd.reqdate,
+              hd.ordflag
+            ORDER BY
+              hd.reqdate;
+            """;
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+}
